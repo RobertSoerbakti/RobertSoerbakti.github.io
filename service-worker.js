87e4f6d09e9d1afb52c8ae095c2ad2ef3af03edc
Robert Soerbakti <rsuranta@dyned.com>
2020-06-17 17:13:55 +0700

    baru

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2891f709d6a1c354fd66879d3657950b.js"
+  "/precache-manifest.f625a88343dbdca2e4c753e7d06507c5.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "agora-webrtc-precall-test"});
+workbox.core.setCacheNameDetails({prefix: "live-reborn-web"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "agora-webrtc-precall-test"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
