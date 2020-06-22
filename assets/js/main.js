f5ed20177d9b851238615c93f918b7a5c31d515c
Robert Soerbakti <rsuranta@dyned.com>
2020-06-22 16:03:00 +0700

    new

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+//Use Strict Mode
+(function($) {
+  "use strict";
+//Begin - Window Load
+$(window).load(function(){
+
+
+  //==============___Page Loader___================
+  
+  $('#page-loader').delay(2000).fadeOut(200, function(){
+
+  });
+
+  $('#loader-name').addClass('loader-left');
+  $('#loader-job').addClass('loader-right');
+  $('#loader-animation').addClass('loader-hide');
+
+});
+
+//Begin - Document Ready
+$(document).ready(function(){
+
+  //==============___Page Loader___================
+  $('#loading-wraper').fadeIn(400);
+
+//==============___Testimonials - owl Carousel___================
+ $("#testimonial-carousel").owlCarousel({
+    navigation : false, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,      
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+    pagination: true,
+    singleItem: true,    
+    navigationText: ["<span class='fa fa-chevron-left'></span>","<span class='fa fa-chevron-right'></span>"],     
+  });
+
+
+//==============___Menu & Pages Animation___================
+
+var linkHome = 0;
+var linkPage = '';
+
+function pageOn(){
+    $('#main-menu').addClass('main-menu-pgactive');
+    $('#section-home').addClass('section-vcardbody-pgactive');    
+    $('.profileActive').removeClass('profileActive');    
+    $('#profile2').addClass('profileActive');
+    
+    linkHome = 1;
+}
+
+function pageOff(){
+    $('.section-page-active').removeClass('section-page-active');
+    $('#main-menu').removeClass('main-menu-pgactive');
+    $('#section-home').removeClass('section-vcardbody-pgactive');
+    $('.profileActive').removeClass('profileActive');
+    $('#profile1').addClass('profileActive');
+    linkHome = 0;
+}
+
+
+$(".link-page").on('click', function(event){
+  event.preventDefault();
+  $('.menuActive').removeClass('menuActive');  
+  $(this).addClass('menuActive');
+  linkPage = $(this).attr('href');
+  $('.section-page-active').removeClass('section-page-active');
+  $(linkPage).addClass('section-page-active');
+  pageOn();
+});
+
+
+$(".link-home").on('click', function(event){
+  event.preventDefault();
+
+  if (linkHome == 0) {
+    //pageOn();
+  }
+  else if (linkHome == 1) {
+    $('.menuActive').removeClass('menuActive');
+    $(this).addClass('menuActive');
+    pageOff();
+  }  
+});
+
+
+
+
+
+
+//End - Document Ready
+});
+
+//End - Use Strict mode
+})(jQuery);
