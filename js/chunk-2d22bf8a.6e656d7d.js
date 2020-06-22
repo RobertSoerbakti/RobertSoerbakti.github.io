e9ab86662b610973d3c8e5eebf59dc11b519c748
Robert Soerbakti <rsuranta@dyned.com>
2020-06-22 11:51:26 +0700

    #filters

--- js/chunk-2d22bf8a.6e656d7d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22bf8a"],{f0f0:function(t,n,e){"use strict";e.r(n);var s=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",[e("ve-line",{attrs:{data:t.data,settings:t.settings,grid:t.grid}})],1)},a=[],i={props:["data","settings","grid"]},r=i,d=e("2877"),l=Object(d["a"])(r,s,a,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d22bf8a.6e656d7d.js.map
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dist/precache-manifest.1ebd68529fee45832bebad97865429ca.js"
+  "/precache-manifest.5a7100b3632f91914532a2949318e32c.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "agora-webrtc-precall-test"});
+workbox.core.setCacheNameDetails({prefix: "live-reborn-web"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "agora-webrtc-precall-test"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
